1af3192e87cd24743f8a506d3e3606b3a41e7417
Theodore Tzirides <tntath@gmail.com>
2021-04-09 22:10:35 +0300

    Created a findARoom method and a scanDate method

--- src/UI/MainMenu.java
+++ src/UI/MainMenu.java
@@ -1,9 +1,16 @@
 package UI;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class MainMenu {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello and welcome to the best exotic Marigold hotel!\n");
         System.out.println("Please select one option:");
@@ -14,46 +21,98 @@ public class MainMenu {
                 4. Admin
                 5.Exit""");
 
-        int userInput = scanIntegerInput();
+        int userInput = scanIntegerInput(1, 5);
         System.out.println("User Input: " + userInput);
 
-
-
+        switch (userInput) {
+            case 1:
+                findARoom();
+            case 2:
+                System.out.println("Case 2");
+                break;
+            case 3:
+                System.out.println("Case 3");
+                break;
+            case 4:
+                System.out.println("Case 4");
+                break;
+            case 5:
+                System.out.println("Case 5");
+                break;
+            default:
+                System.out.println("Ending program");
+        }
 
 
     }
 
+
     /**
      * A method that uses the Scanner class to get an integer input
-     * from the user. Handles exceptions of invalid input or out of limits input.
+     * from the user. The integer must be withing a lower and an upper limit.
+     * Handles exceptions of invalid input or out of limits input.
+     *
+     * @param lowerLimit the lower limit of the integer input
+     * @param upperLimit the upper limit of the integer input
      * @return the integer retrieved from the input
      */
-    public static int scanIntegerInput(){
+    public static int scanIntegerInput(int lowerLimit, int upperLimit) {
+        Scanner scanner = new Scanner(System.in);
         int userInput = 0;
         boolean wrongInput;
-        Scanner scanner = new Scanner(System.in);
 
         do {
             try {
                 userInput = scanner.nextInt();
-                if (userInput >= 1 && userInput <= 5) {
+                if (userInput >= lowerLimit && userInput <= upperLimit) {
                     wrongInput = false;
                 } else {
                     wrongInput = true;
-                    System.out.println("Please enter a number between 1 and 5.");
+                    System.out.println("Please enter a number between " + lowerLimit + " and " + upperLimit);
                 }
             } catch (Exception ex) {
                 ex.getLocalizedMessage();
-                System.out.println("Please enter a valid number");
+                System.out.println("Please enter an integer number");
                 wrongInput = true;
-                scanner.nextLine();
+                String date = scanner.nextLine();
             }
         } while (wrongInput);
 
-        scanner.close();
-
         return userInput;
 
+    }
+
+    public static void findARoom() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        System.out.println("Please enter the check in date in dd-mm-yyyy format");
+        LocalDate checkInDate = scanDate(formatter);
+        System.out.println("Check In: " + checkInDate.format(formatter));
+
+        System.out.println("Please enter the check out date in dd-mm-yyyy format");
+        LocalDate checkOutDate = scanDate(formatter);
+        System.out.println("Check Out: " + checkOutDate.format(formatter));
+
+
+    }
+
+    public static LocalDate scanDate(DateTimeFormatter formatter){
+        Scanner scanner = new Scanner(System.in);
+        LocalDate inputDate = LocalDate.now();
+        boolean wrongInput;
+        do {
+            try {
+                String date = scanner.nextLine();
+                inputDate = LocalDate.parse(date, formatter);
+                wrongInput = false;
+            } catch (Exception ex) {
+                ex.getLocalizedMessage();
+                System.out.println("Please enter the correct date format \"dd-mm-yyyy\"");
+                wrongInput = true;
+            }
+        } while (wrongInput);
+
+        return inputDate;
     }
 
 }
