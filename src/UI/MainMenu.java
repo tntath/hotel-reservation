d02a12f816d43a1cc843ced898a4848b8a557b0e
Theodore Tzirides <tntath@gmail.com>
2021-04-09 13:27:13 +0300

    moved code for scanner in a separate method and replaced string concatenation with text block

--- src/UI/MainMenu.java
+++ src/UI/MainMenu.java
@@ -4,23 +4,39 @@ import java.util.Scanner;
 
 public class MainMenu {
     public static void main(String[] args) {
-        boolean wrongInput = false;
-        int userInput = 0;
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello and welcome to the best exotic Marigold hotel!\n");
         System.out.println("Please select one option:");
-        System.out.println("1. Find and reserve a room \n" +
-                "2. See my reservations\n" +
-                "3. Create an account \n" +
-                "4. Admin\n" +
-                "5.Exit");
+        System.out.println("""
+                1. Find and reserve a room\s
+                2. See my reservations
+                3. Create an account\s
+                4. Admin
+                5.Exit""");
+
+        int userInput = scanIntegerInput();
+        System.out.println("User Input: " + userInput);
+
+
+
+
+
+    }
+
+    /**
+     * A method that uses the Scanner class to get an integer input
+     * from the user. Handles exceptions of invalid input or out of limits input.
+     * @return the integer retrieved from the input
+     */
+    public static int scanIntegerInput(){
+        int userInput = 0;
+        boolean wrongInput;
+        Scanner scanner = new Scanner(System.in);
 
         do {
             try {
                 userInput = scanner.nextInt();
                 if (userInput >= 1 && userInput <= 5) {
-                    System.out.println("User Input: " + userInput);
                     wrongInput = false;
                 } else {
                     wrongInput = true;
@@ -34,11 +50,9 @@ public class MainMenu {
             }
         } while (wrongInput);
 
-        switch(userInput) {
-            case 1:
+        scanner.close();
 
-        }
-
+        return userInput;
 
     }
 
