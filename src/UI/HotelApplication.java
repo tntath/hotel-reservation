4b725844b17f38a3677122a53aec32318370d1aa
Theodore Tzirides <tntath@gmail.com>
2021-04-13 19:55:04 +0300

    Created main method in HotelApplication

--- src/UI/HotelApplication.java
+++ src/UI/HotelApplication.java
@@ -1,2 +1,18 @@
-package UI;public class HotelApplication {
+package UI;
+
+import java.util.Scanner;
+import static UI.MainMenu.*;
+
+public class HotelApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //boolean programRunning = true;
+
+        System.out.println("Hello and welcome to the best exotic Marigold hotel!\n");
+        System.out.println("Please enter your email");
+        String customerEmail = scanner.nextLine();
+        selectOption(customerEmail);
+
+
+    }
 }
