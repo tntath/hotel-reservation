99719ec581dfb97c2451020e98d557801a64915c
Theodore Tzirides <tntath@gmail.com>
2021-04-13 21:15:05 +0300

    Restructured RoomType enum to print a label

--- src/model/RoomType.java
+++ src/model/RoomType.java
@@ -1,6 +1,12 @@
 package model;
 
 public enum RoomType {
-    SINGLE,
-    DOUBLE
+    SINGLE("Single"),
+    DOUBLE("Double");
+
+    public final String label;
+
+    RoomType(String label) {
+        this.label = label;
+    }
 }
